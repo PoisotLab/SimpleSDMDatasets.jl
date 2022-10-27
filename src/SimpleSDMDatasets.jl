24ccabfd26b2dca2e4716656759c4a99616d7b5b
Timothée Poisot (he/him) <timothee.poisot@umontreal.ca>
2022-10-27 19:59:49 -0400

    🦆 addnl futures for CHELSA models

--- src/SimpleSDMDatasets.jl
+++ src/SimpleSDMDatasets.jl
@@ -15,13 +15,26 @@ const _LAYER_PATH =
     get(ENV, "SDMLAYERS_PATH", joinpath(_data_storage_folders, "SimpleSDMDatasets"))
 isdir(_LAYER_PATH) || mkpath(_LAYER_PATH)
 
-# Types for datasets
+# Types for datasets and providers
 include(joinpath(@__DIR__, "types", "datasets.jl"))
-include(joinpath(@__DIR__, "types", "providers.jl"))
-export RasterProvider, RasterDataset
+export RasterDataset
 export BioClim, Elevation, MinimumTemperature, MaximumTemperature, AverageTemperature,
     Precipitation, SolarRadiation, WindSpeed, WaterVaporPressure, LandCover
+
+include(joinpath(@__DIR__, "types", "providers.jl"))
+export RasterProvider
 export WorldClim, EarthEnv, CHELSA
+
+include(joinpath(@__DIR__, "types", "futures.jl"))
+export FutureScenario, FutureModel
+
+# CMIP6 exports
+export CMIP6Scenario, CMIP6Model
+export SSP126, SSP245, SSP370, SSP585
+export ACCESS_CM2, ACCESS_ESM1_5, BCC_CSM2_MR, CanESM5, CanESM5_CanOE, CMCC_ESM2,
+    CNRM_CM6_1, CNRM_CM6_1_HR, CNRM_ESM2_1, EC_Earth3_Veg, EC_Earth3_Veg_LR, FIO_ESM_2_0,
+    GFDL_ESM4, GISS_E2_1_G, GISS_E2_1_H, HadGEM3_GC31_LL, INM_CM4_8, INM_CM5_0,
+    IPSL_CM6A_LR, MIROC_ES2L, MIROC6, MPI_ESM1_2_LR, MPI_ESM1_2_HR, MPI_ESM2_0, UKESM1_0_LL
 
 # Common interface
 include(joinpath(@__DIR__, "types", "interface.jl"))
--- src/types/futures.jl
+++ src/types/futures.jl
@@ -34,5 +34,6 @@ struct IPSL_CM6A_LR <: CMIP6Model end
 struct MIROC_ES2L <: CMIP6Model end
 struct MIROC6 <: CMIP6Model end
 struct MPI_ESM1_2_LR <: CMIP6Model end
+struct MPI_ESM1_2_HR <: CMIP6Model end
 struct MPI_ESM2_0 <: CMIP6Model end
 struct UKESM1_0_LL <: CMIP6Model end
